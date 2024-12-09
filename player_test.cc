4dcbc1de93cd12cf2e844118d2959e509ba9a8b7
Elvira Eke <elvek164@student.liu.se>
2024-12-09 10:08:49 +0100

    börjat på testfil för player

--- player_test.cc
+++ player_test.cc
@@ -1,9 +1,27 @@
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+
 #include "catch.hpp"
 #include "player.h"
+#include "game_object.h"
+#include "player.h"
+#include "context.h"
+#include "snowball_projectile.h"
+#include "static_obstacle.h"
+#include "temporary_modifier.h"
 
 
-
-TEST_CASE("Player")
+TEST_CASE("Player perform collision")
 {
-    Player player(1,2,3,4);
+    Context context {};
+    context.player = new Player(40, 50);
+
+    Tire tire (30, 40, 130);
+
+    sf::FloatRect player_bounds{context.player -> bounds()};
+    sf::FloatRect obst_bounds{tire.bounds()};
+    CHECK(player_bounds.intersects(obst_bounds));
 }
+
